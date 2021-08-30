d7abf4da5cc3c182104fd76c9e02357c0d86a781
BimoSora <bimosora3@gmail.com>
2021-08-30 08:54:15 +0700

    Update help.js

--- help.js
+++ help.js
@@ -28,26 +28,20 @@ Anda dapat menghapus file 4 cara.
   ⚫ Hapus semua file Kirim ke BOT.
 
 ~ Hapus file individual dengan file_id.
-<b>/rem</b> file_id.
-<b>Ini akan menghapus file satu per satu saat Anda memberikan file_id, dapatkan file_id dari saluran log.</b>
+<b>/rem</b> file_id. Ini akan menghapus file satu per satu saat Anda memberikan file_id, dapatkan file_id dari saluran log.
 
 ~ Hapus file group dengan mediaId.
-<b>/remgrp</b> mediaId.
-<b>Ini akan menghapus media dalam group, dapatkan mediaId dari saluran log).</b>
+<b>/remgrp</b> mediaId. Ini akan menghapus media dalam group, dapatkan mediaId dari saluran log.
 
 ~ Hapus semua file Kirim oleh pengguna.
-<b>/remall</b> userID.
-<b>Anda dapat menghapus semua file dikirim oleh pengguna tertentu jika pengguna mengirim konten atau spam yang kasar, dapatkan userid dari saluran log).</b>
+<b>/remall</b> userID. Anda dapat menghapus semua file dikirim oleh pengguna tertentu jika pengguna mengirim konten atau spam yang kasar, dapatkan userid dari saluran log.
 
 ~ Hapus semua file Kirim ke B0T.
-<b>/clear</b>
-<b>Ini akan menghapus semua file yang dikirim ke BOT secara permanen).</b>
+<b>/clear</b>. Ini akan menghapus semua file yang dikirim ke BOT secara permanen.
 
 <b>Kirim pesan ke pengguna</b>
-<b>/broadcast</b> Pesan Anda akan dikirim ke pengguna.
-<b>Anda dapat menyiarkan pesan teks ke pengguna Anda, pesan akan dikirim dari pengguna terakhir bergabung untuk pertama-tama bergabung dengan pengguna untuk mengurangi spam. Cobalah untuk tidak mengirim terlalu banyak pesan sekaligus jika Anda memiliki sejumlah besar pengguna).</b>
+<b>/broadcast</b>. Anda dapat menyiarkan pesan teks ke pengguna Anda, pesan akan dikirim dari pengguna terakhir bergabung untuk pertama-tama bergabung dengan pengguna untuk mengurangi spam. Cobalah untuk tidak mengirim terlalu banyak pesan sekaligus jika Anda memiliki sejumlah besar pengguna.
 
 <b>Cara Mengetahui Total Pengguna BOT.</b>
-<b>/stats</b>
-<b>Anda akan mendapatkan total pengguna memulai BOT Anda, data waktu nyata akan diperbarui setelah siaran yang berhasil.</b>
+<b>/stats</b>. Anda akan mendapatkan total pengguna memulai BOT Anda, data waktu nyata akan diperbarui setelah siaran yang berhasil.
 `;
