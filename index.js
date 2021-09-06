338de3a6a73ee3508a7cd30b93f96b26403d2e5e
BimoSora <bimosora3@gmail.com>
2021-09-06 13:02:41 +0700

    first commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const mediaLimitConfig = {
     onLimitExceeded: (ctx, next) => ctx.reply('Silakan menunggu 1 menit untuk mengirim lagi, minimal 20 pesan sekali kirim')
 }
 const bot = new Telegraf(process.env.TOKEN)
-bot.use(rateLimit(if(ctx.chat.type == 'private') {limitConfig}))
+bot.use(rateLimit(limitConfig))
 
 process.env.TZ = "Asia/Jakarta";
 
