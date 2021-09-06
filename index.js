30a7eb12a57877a8ad67c63d4941d559f66b15fa
BimoSora <bimosora3@gmail.com>
2021-09-06 08:07:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 require('dotenv').config()
 const { Telegraf } = require('telegraf')
-const crypto = require('crypto');
+const { rateLimit } = require('telegraf-ratelimit')
+const { crypto } = require('crypto');
+const limitConfig = {
+  window: 10000,
+  limit: 20,
+  onLimitExceeded: (ctx, next) => ctx.reply('Silakan menunggu 10 detik untuk mengirim lagi, minimal 20 pesan sekali kirim')
+}
 const bot = new Telegraf(process.env.TOKEN)
+bot.use(rateLimit(limitConfig))
 
 process.env.TZ = "Asia/Jakarta";
 
