5705a9ff24af7052341f782dc7fffba8cd156a51
BimoSora <bimosora3@gmail.com>
2021-10-20 17:19:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ require('dotenv').config()
 const { Telegraf } = require('telegraf')
 const rateLimit = require('telegraf-ratelimit')
 const crypto = require('crypto')
-//const limitConfig = {
-//    window: 3000,
-//    limit: 20,
-//    onLimitExceeded: (ctx, next) => {
-//        if(ctx.chat.type == 'private') {
-//            ctx.reply('Silakan menunggu 3 detik untuk mengirim, maksimal 20 pesan sekali kirim')
-//        }
-//    }
-//}
+const limitConfig = {
+    window: 3000,
+    limit: 1,
+    onLimitExceeded: (ctx, next) => {
+        if(ctx.chat.type == 'private') {
+            ctx.reply('Silakan menunggu 3 detik untuk mengirim, maksimal 20 pesan sekali kirim')
+        }
+    }
+}
 const mediaLimitConfig = {
     window: 60000,
     limit: 20,
@@ -24,7 +24,7 @@ const mediaLimitConfig = {
     }
 }
 const bot = new Telegraf(process.env.TOKEN)
-//bot.use(rateLimit(limitConfig))
+bot.use(rateLimit(limitConfig))
 
 process.env.TZ = "Asia/Jakarta";
 
