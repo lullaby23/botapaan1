bfd320d116f197bd186a5006d1bb4d193515b96c
BimoSora <bimosora3@gmail.com>
2021-11-09 13:39:50 +0700

    first commit

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ bot.start(async(ctx)=>{
                             }
                             return ctx.telegram.sendMediaGroup(ctx.chat.id, mediagroup) + setTimeout(captionFunction, 1000)
                         })
-                    }catch(error){
+                    }catch(err){
                         ctx.reply(`Media tidak ditemukan atau sudah dihapus`)
                     }
                 }else{
