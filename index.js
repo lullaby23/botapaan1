92a230085c9fc181c20e541972b833ac9d57f59e
BimoSora <bimosora3@gmail.com>
2021-12-24 14:07:54 +0700

    first commit

--- index.js
+++ index.js
@@ -146,12 +146,12 @@ bot.start(async(ctx, next)=>{
                             mediagroup.push({type: data.type, media: data.file_id, caption: data.caption, parse_mode:'HTML'});
                         }
 
-                        await ctx.deleteMessage()
                         async function captionFunction() {
                             return await ctx.reply(`${captionbuild(ctx)}`,{
                                 parse_mode:'HTML'
                             })
                         }
+                        await ctx.deleteMessage()
                         await ctx.telegram.sendMediaGroup(ctx.chat.id, mediagroup);
                         setTimeout(captionFunction, 1000)
                     }catch(error){
@@ -279,7 +279,6 @@ bot.start(async(ctx, next)=>{
                                         mediagroup.push({type: data.type, media: data.file_id, caption: data.caption, parse_mode:'HTML'});
                                     }
                                 
-                                    await ctx.deleteMessage()
                                     async function captionFunction() {
                                         return await ctx.reply(`${captionbuild(ctx)}`,{
                                             parse_mode:'HTML'
@@ -294,6 +293,7 @@ bot.start(async(ctx, next)=>{
                                                 await ctx.reply(`${messagebanned(ctx)}`)
                                             }
                                         }else{
+                                            await ctx.deleteMessage()
                                             await ctx.telegram.sendMediaGroup(ctx.chat.id, mediagroup);
                                             setTimeout(captionFunction, 1000)
                                         }
