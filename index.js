58b9bb00a655b8fbd23a9c6878d74f50f84c0b0b
lullaby23 <84791701+lullaby23@users.noreply.github.com>
2022-02-07 14:30:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -86,6 +86,11 @@ const url4 = url2[1];
 
 // inline keyboard
 const inKey = [
+    [{text:'📎 Tautan',callback_data:'POP'}],
+
+    [{text:'📚 Dokumentasi',callback_data:'DOC'},{text:'🆘 Bantuan',callback_data:'HELP'}],
+
+    [{text:'💿 Source code',callback_data:'SRC'}],
         [{text: `${url3}`, url: `${url4}`}]
 ];
 
