ef3382a97ec2d6947a5b01f6089a16205e1e0e2c
lullaby23 <84791701+lullaby23@users.noreply.github.com>
2022-02-28 04:37:33 +0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ const state = {
 
 module.exports.connect = function (done) {
         const url = process.env.DB_URL
-        const dbname = 'mediaFilw'
+        const dbname = 'mediaFile'
 
     MongoClient.connect(url,{ useUnifiedTopology: true } , (err, data) => {
         if (err) return done(err)
